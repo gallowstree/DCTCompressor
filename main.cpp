6ee07b1dfe644b8cb3a3da7cec2e52ff9b247aad
Alejandro Alvarado <alejandroalvarado@iMac-de-Alejandro.local>
2014-09-10 17:10:20 -0600

    WIP quantization, tests, etc...

--- main.cpp
+++ main.cpp
@@ -3,25 +3,38 @@
 #include "stdio.h"
 #include "ImageMatrix.h"
 #include <sys/time.h>
+
 //#define DEBUG
 
 using namespace std;
 
 vector< vector<double> > aux;
 
-int sub_matrix_size;
+const int sub_matrix_size = 8;
+vector< vector < double > > dct_mat =
+{
+    {0.3536,  0.3536,  0.3536,  0.3536,  0.3536,  0.3536,  0.3536,  0.3536},
+    {0.4904,  0.4157,  0.2778,  0.0975, -0.0975, -0.2778, -0.4157, -0.4904},
+    {0.4619,  0.1913, -0.1913, -0.4619, -0.4619, -0.1913,  0.1913 , 0.4619},
+    {0.4157, -0.0975, -0.4904, -0.2778,  0.2778,  0.4904,  0.0975, -0.4157},
+    {0.3536, -0.3536, -0.3536,  0.3536,  0.3536, -0.3536, -0.3536,  0.3536},
+    {0.2778, -0.4904,  0.0975,  0.4157, -0.4157, -0.0975,  0.4904, -0.2778},
+    {0.1913, -0.4619,  0.4619, -0.1913, -0.1913,  0.4619, -0.4619,  0.1913},
+    {0.0975, -0.2778,  0.4157, -0.4904,  0.4904, -0.4157,  0.2778 ,-0.0975}
+};
 
-double get_wall_time(){
-    struct timeval time;
-    if (gettimeofday(&time,NULL)){
-        //  Handle error
-        return 0;
-    }
-    return (double)time.tv_sec + (double)time.tv_usec * .000001;
-}
-double get_cpu_time(){
-    return (double)clock() / CLOCKS_PER_SEC;
-}
+vector<vector<int>> quantiztion_50 =
+{
+    {16,  11,  10,  16,  24,  40,  51,  61},
+    {12,  12,  14,  19,  26,  58,  60,  55},
+    {14,  13,  16,  24,  40,  57,  69,  56},
+    {14,  17,  22,  29,  51,  87,  80,  62},
+    {18,  22,  37,  56,  68, 109, 103,  77},
+    {24,  35,  55,  64,  81, 104, 113,  92},
+    {49,  64,  78,  87, 103, 121, 120,  101},
+    {72,  92,  95,  98, 112, 100, 103,  99}
+};
+
 
 void fill_aux(int row_start, int col_start, ImageMatrix* img)
 {
@@ -36,15 +49,15 @@ void fill_aux(int row_start, int col_start, ImageMatrix* img)
     }
 }
 
-
-void init_square_mat(int size, vector<vector<double>> &mat)
+template<class T>
+void init_square_mat(int size, vector<vector<T>> &mat)
 {
     mat.reserve(size);
     for ( int i = 0 ; i < size ; i++ )
         mat[i].resize(size);
 }
 
-vector<vector<double>> mult_square_mat(vector<vector<double>> &mat1, vector<vector<double>> &mat2)
+vector<vector<double>> mult_square_mat(vector<vector<double>> &mat1, vector<vector<double>> &mat2, bool div)
 {
     int size = mat1.size();
     vector<vector<double>> product(size);
@@ -55,14 +68,15 @@ vector<vector<double>> mult_square_mat(vector<vector<double>> &mat1, vector<vect
         {
             for(int col = 0; col < size; col++)
             {
-                product[row][r_col] += mat1[row][col]*mat2[col][r_col];
+                product[row][r_col] += div ? mat1[row][col]/mat2[col][r_col] : mat1[row][col]*mat2[col][r_col];
             }
         }
     }
     return product;
 }
 
-vector<vector<double>> transpose(vector<vector<double>> &mat)
+template<class T>
+vector<vector<double>> transpose(vector<vector<T>> &mat)
 {
     int r_size = mat.size();
     int c_size = mat[0].size();
@@ -73,24 +87,36 @@ vector<vector<double>> transpose(vector<vector<double>> &mat)
     return outtrans;
 }
 
-int main()
+vector<vector<int>> quantize(vector<vector<double>> &mat)
 {
-    sub_matrix_size = 8;
-    init_square_mat(sub_matrix_size, aux);
-    ImageMatrix* img = new ImageMatrix("/Users/alejandroalvarado/cc4/DCTCompressor/Images/lena512.bmp");
+    vector<vector<int>> q(sub_matrix_size);
+    init_square_mat(sub_matrix_size,q);
     
-    vector< vector < double > > dct_mat =
-    {
-        {0.3536,  0.3536,  0.3536,  0.3536,  0.3536,  0.3536,  0.3536,  0.3536},
-        {0.4904,  0.4157,  0.2778,  0.0975, -0.0975, -0.2778, -0.4157, -0.4904},
-        {0.4619,  0.1913, -0.1913, -0.4619, -0.4619, -0.1913,  0.1913 , 0.4619},
-        {0.4157, -0.0975, -0.4904, -0.2778,  0.2778,  0.4904,  0.0975, -0.4157},
-        {0.3536, -0.3536, -0.3536,  0.3536,  0.3536, -0.3536, -0.3536,  0.3536},
-        {0.2778, -0.4904,  0.0975,  0.4157, -0.4157, -0.0975,  0.4904, -0.2778},
-        {0.1913, -0.4619,  0.4619, -0.1913, -0.1913,  0.4619, -0.4619,  0.1913},
-        {0.0975, -0.2778,  0.4157, -0.4904,  0.4904, -0.4157,  0.2778 ,-0.0975}
-    };
+    return q;
+}
+
+
+void dct_compress(ImageMatrix *img)
+{
     vector< vector < double > > dct_t = transpose(dct_mat);
+    init_square_mat(sub_matrix_size, aux);
+    vector<vector<double>> prod;
+    for(int row = 0; row< (int)(img->width); row+=sub_matrix_size)
+    {
+        for(int col = 0; col < (int)(img->width); col+=sub_matrix_size)
+        {
+            fill_aux(row, col, img);
+            prod = mult_square_mat(dct_mat, aux, false);
+            //prod = mult_square_mat(dct_mat, test_mat);
+            prod = mult_square_mat(prod, dct_t, false);
+            quantize(prod);
+        }
+    }
+}
+
+
+void test()
+{
     vector< vector < double > > test_mat =
     {
         {26, -5,  -5,  -5, -5, -5, -5,  8},
@@ -102,26 +128,44 @@ int main()
         {-5, -23,  -18, 21, 8,  8, 52,  38},
         {-18, 8,  -5, -5,  -5, 8,  26 ,8}
     };
-    
-    double wall0 = get_wall_time();
-    double cpu0  = get_cpu_time();
-    vector<vector<double>> prod;
-    for(int row = 0; row< (int)(img->width); row+=sub_matrix_size)
-    {
-        for(int col = 0; col < (int)(img->width); col+=sub_matrix_size)
+    vector< vector < double > > dct_t = transpose(dct_mat);
+    vector<vector<double>> prod = mult_square_mat(dct_mat, test_mat, false);
+    prod = mult_square_mat(prod, dct_t, false);
+    for(int r = 0; r < 8; r++){
+        for(int c = 0; c < 8; c++)
         {
-            fill_aux(row, col, img);
-            prod = mult_square_mat(dct_mat, aux);
-            //prod = mult_square_mat(dct_mat, test_mat);
-            prod = mult_square_mat(prod, dct_t);
+            printf("%7.3f ", prod[r][c]);
         }
-
+        printf("\n");
     }
-    double wall1 = get_wall_time();
+
+}
+
+double get_total_time()
+{
+    struct timeval time;
+    gettimeofday(&time,NULL);
+    return (double)time.tv_sec + (double)time.tv_usec * .000001;
+}
+double get_cpu_time(){
+    return (double)clock() / CLOCKS_PER_SEC;
+}
+
+int main()
+{
+    ImageMatrix* img = new ImageMatrix("/Users/alejandroalvarado/cc4/DCTCompressor/Images/lena512.bmp");
+
+    
+    double wall0 = get_total_time();
+    double cpu0  = get_cpu_time();
+    dct_compress(img);
+    double wall1 = get_total_time();
     double cpu1  = get_cpu_time();
     
-    cout << "Wall Time = " << wall1 - wall0 << endl;
-    cout << "CPU Time  = " << cpu1  - cpu0  << endl;
+    test();
+    
+    cout << "Tiempo total = " << wall1 - wall0 << endl;
+    cout << "Tiempo CPU   = " << cpu1  - cpu0  << endl;
 
     
     return 0;

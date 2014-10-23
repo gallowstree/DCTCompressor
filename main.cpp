f3935ddb1fc14d2e2b02520b2528b00b8d3f9931
Alejandro Alvarado <alejandroalvarado@iMac-de-Alejandro.local>
2014-10-23 15:10:52 -0600

    CLI y calidad variable

--- main.cpp
+++ main.cpp
@@ -21,13 +21,34 @@ double get_cpu_time()
     return (double)clock() / CLOCKS_PER_SEC;
 }
 
+void print_help(string name)
+{
+    cout << name << " -c path/to/input path/to/output quality "<<  endl;
+    cout << name << " -d path/to/input path/to/output quality "<<  endl;
+}
 
-int main()
+//1: -c(ompress)||-d(ecompress)
+//2: input file
+//3: output file
+//4: quality
+int main(int argc, char* argv[])
 {
     //test();
+    if(argc != 5)
+    {
+        print_help(argv[0]);
+        return -1;
+    }
+
     DCTCompressor* compressor = new DCTCompressor();
-    compressor->compress_image("Images/lena512.bmp","compressed.bin",50);
-    compressor->decompress_image("compressed.bin","reconstructed.bmp",50);
+    
+    if(strcmp(argv[1],"-c") == 0)
+        compressor->compress_image(argv[2],argv[3], stoi( argv[4] ));
+    else if(strcmp(argv[1],"-d") == 0)
+        compressor->decompress_image(argv[2],argv[3], stoi( argv[4] ));
+    else
+        print_help(argv[0]);
+    
     delete compressor;
     return 0;
 }
